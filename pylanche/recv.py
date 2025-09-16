02b3b8eba29dd505931732344fb84216aed65a9a
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-09-16 11:52:33 +0000

    Initialize recv script

---
+++ pylanche/recv.py
@@ -0,0 +1,36 @@
+import asyncio
+import json
+
+from azure.eventhub.aio import EventHubConsumerClient
+
+with open('./pylanche/event_hub.json', 'r') as file:
+    event_hub_config = json.load(file)
+    EVENT_HUB_CONNECTION_STR = event_hub_config['EVENT_HUB_CONNECTION_STR']
+    EVENT_HUB_NAME = event_hub_config['EVENT_HUB_NAME']
+
+async def on_event(partition_context, event):
+    # Print the event data.
+    print(
+        'Received the event: "{}" from the partition with ID: "{}"'.format(
+            event.body_as_str(encoding="UTF-8"), partition_context.partition_id
+        )
+    )
+
+    # Update the checkpoint so that the program doesn't read the events
+    # that it has already read when you run it next time.
+    await partition_context.update_checkpoint(event)
+
+async def main():
+    # Create a consumer client for the event hub.
+    client = EventHubConsumerClient.from_connection_string(
+        EVENT_HUB_CONNECTION_STR,
+        consumer_group="$Default",
+        eventhub_name=EVENT_HUB_NAME
+    )
+    async with client:
+        # Call the receive method. Read from the beginning of the
+        # partition (starting_position: "-1")
+        await client.receive(on_event=on_event, starting_position="-1")
+
+if __name__ == "__main__":
+    asyncio.run(main())
