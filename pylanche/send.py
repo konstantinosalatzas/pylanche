2fb6875c388912579458b0e3a6f1bc07776bb744
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-09-17 13:13:54 +0000

    Change counter range

--- pylanche/send.py
+++ pylanche/send.py
@@ -15,8 +15,8 @@ async def run(EVENT_HUB_CONNECTION_STR: str, EVENT_HUB_NAME: str, SEND_COUNT: in
         event_data_batch = await producer.create_batch()
 
         # Add events to the batch.
-        for i in range(1, SEND_COUNT+1):
-            event_data_batch.add(EventData(f"Event {str(i)}"))
+        for i in range(0, SEND_COUNT):
+            event_data_batch.add(EventData(f"Event {str(i+1)}"))
 
         # Send the batch of events to the event hub.
         await producer.send_batch(event_data_batch)
