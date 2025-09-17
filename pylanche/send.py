05f53ccf073ee23a5ae37ee983a9d681cde6ca62
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-09-17 13:10:38 +0000

    Send the specified number of events

--- pylanche/send.py
+++ pylanche/send.py
@@ -3,7 +3,7 @@ import asyncio
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
 
-async def run(EVENT_HUB_CONNECTION_STR: str, EVENT_HUB_NAME: str):
+async def run(EVENT_HUB_CONNECTION_STR: str, EVENT_HUB_NAME: str, SEND_COUNT: int):
     # Create a producer client to send messages to the event hub.
     # Specify a connection string to your event hubs namespace and
     # the event hub name.
@@ -15,12 +15,11 @@ async def run(EVENT_HUB_CONNECTION_STR: str, EVENT_HUB_NAME: str):
         event_data_batch = await producer.create_batch()
 
         # Add events to the batch.
-        event_data_batch.add(EventData("First event"))
-        event_data_batch.add(EventData("Second event"))
-        event_data_batch.add(EventData("Third event"))
+        for i in range(1, SEND_COUNT+1):
+            event_data_batch.add(EventData(f"Event {str(i)}"))
 
         # Send the batch of events to the event hub.
         await producer.send_batch(event_data_batch)
 
-def send(EVENT_HUB_CONNECTION_STR: str, EVENT_HUB_NAME: str):
-    asyncio.run(run(EVENT_HUB_CONNECTION_STR, EVENT_HUB_NAME))
+def send(EVENT_HUB_CONNECTION_STR: str, EVENT_HUB_NAME: str, SEND_COUNT: int):
+    asyncio.run(run(EVENT_HUB_CONNECTION_STR, EVENT_HUB_NAME, SEND_COUNT))
