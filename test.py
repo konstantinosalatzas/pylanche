45fc2a06afbd0fc5eed979d867fdf63b16d45d55
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-09-17 09:44:27 +0000

    Initialize tests

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+import pylanche
+
+
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        self.client = pylanche.client()
+
+    def test_receive(self):
+        self.client.receive()
+
+    def test_send(self):
+        self.client.send()
+
+if __name__ == "__main__":
+    unittest.main()
