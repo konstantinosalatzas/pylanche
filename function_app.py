5fe9323ce3d4224d8d65b02b63994a58635130f5
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-09-19 07:33:10 +0000

    Change quotation marks

--- function_app.py
+++ function_app.py
@@ -7,7 +7,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
     op = req.params.get('operation')
     if not op:
@@ -30,14 +30,14 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             client.receive()
         except Exception as error:
             logging.error(str(error))
-        return func.HttpResponse('The function received events from the event hub.')
+        return func.HttpResponse("The function received events from the event hub.")
     elif op == "send":
         try:
             # Send events to the event hub.
             client.send()
         except Exception as error:
             logging.error(str(error))
-        return func.HttpResponse('The function sent events to the event hub.')
+        return func.HttpResponse("The function sent events to the event hub.")
     else:
         return func.HttpResponse(
              "Pass an operation in the query string or in the request body to send or receive events.",
