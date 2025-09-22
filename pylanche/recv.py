769a52aa310b4558dfa5d0503174c2996ab5caf8
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-09-22 13:07:59 +0000

    Return parsed JSON dict

--- pylanche/recv.py
+++ pylanche/recv.py
@@ -4,13 +4,18 @@ import logging
 
 from azure.eventhub.aio import EventHubConsumerClient
 
-def process(message: str):
+def process(message: str) -> dict | None:
     try:
         data = json.loads(message)
     except ValueError: # The message is not in JSON format.
-        return
+        print("Failed to parse the message as JSON.")
+        logging.info("Failed to parse the message as JSON.")
+        return None
+    
     print('Processed the event: "{}"'.format(str(data)))
     logging.info('Processed the event: "{}"'.format(str(data)))
+    
+    return data
 
 async def on_event(partition_context, event):
     message = event.body_as_str(encoding="UTF-8")
