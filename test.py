7a5d23f4532df9b16d9dd67b79345b2568152d8b
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-09-23 11:18:15 +0000

    Test parse()

--- test.py
+++ test.py
@@ -12,5 +12,14 @@ class TestClient(unittest.TestCase):
     def test_send(self):
         self.producer.perform(op="send")
 
+class TestProcess(unittest.TestCase):
+    def test_parse_valid(self):
+        message = '{"id": "0"}' # input JSON message
+        data_ans = {'id': "0"} # expected dict
+
+        data_out = pylanche.process.parse(message) # output dict
+
+        self.assertEqual(data_out, data_ans)
+
 if __name__ == "__main__":
     unittest.main()
