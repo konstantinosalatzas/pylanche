766d81d70b1b609084d82d09e9cc9e754ddcb3b3
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-09-28 08:49:40 +0000

    Clean up tests

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ from function_app import http_trigger
 
 class TestFunction(unittest.TestCase):
     def test_http_trigger_receive(self):
-        request = func.HttpRequest(method='POST',
+        request = func.HttpRequest(method="POST",
                                    body = None,
-                                   url='/api/http_trigger',
-                                   params = {'operation': 'receive'}
+                                   url="/api/http_trigger",
+                                   params = {"operation": "receive"}
         ) # input request
 
         f = http_trigger.build().get_user_function()
@@ -18,10 +18,10 @@ class TestFunction(unittest.TestCase):
         print(response.status_code)
 
     def test_http_trigger_send(self):
-        request = func.HttpRequest(method='POST',
+        request = func.HttpRequest(method="POST",
                                    body = None,
-                                   url='/api/http_trigger',
-                                   params = {'operation': 'send'}
+                                   url="/api/http_trigger",
+                                   params = {"operation": "send"}
         ) # input request
 
         f = http_trigger.build().get_user_function()
@@ -29,23 +29,10 @@ class TestFunction(unittest.TestCase):
         response = f(request) # output response
         print(response.status_code)
 
-'''
-class TestClient(unittest.TestCase):
-    def setUp(self):
-        self.consumer = pylanche.Client(op="receive")
-        self.producer = pylanche.Client(op="send")
-
-    def test_receive(self):
-        self.consumer.perform(op="receive")
-
-    def test_send(self):
-        self.producer.perform(op="send")
-'''
-
 class TestProcess(unittest.TestCase):
     def test_parse_valid(self):
         message = '{"id": "0"}' # input JSON message
-        data_ans = {'id': "0"} # expected dict
+        data_ans = {"id": "0"} # expected dict
 
         data_out = pylanche.process.parse(message) # output dict
 
