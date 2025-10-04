d3e9a89002bc0562b625de4c12c708730c91b9ef
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-10-04 14:34:36 +0000

    Rename config dict

--- pylanche/Client.py
+++ pylanche/Client.py
@@ -11,27 +11,27 @@ class Client:
     def __init__(self, op: str):
         # Read the configuration file.
         with open("./pylanche/config.json", "r") as file:
-            event_hub_config = json.load(file)               
+            config = json.load(file)               
 
         if op == "receive":
             # Create an Azure blob checkpoint store to store the checkpoints.
             checkpoint_store = BlobCheckpointStore.from_connection_string(
-                event_hub_config['BLOB_STORAGE_CONN_STR'], event_hub_config['BLOB_CONTAINER_NAME']
+                config['BLOB_STORAGE_CONN_STR'], config['BLOB_CONTAINER_NAME']
             )
             # Create a consumer client to receive events from the event hub.
             self.consumer = EventHubConsumerClient.from_connection_string(
-                event_hub_config['EVENT_HUB_CONN_STR'],
+                config['EVENT_HUB_CONN_STR'],
                 consumer_group="$Default",
-                eventhub_name=event_hub_config['EVENT_HUB_NAME'],
+                eventhub_name=config['EVENT_HUB_NAME'],
                 checkpoint_store=checkpoint_store
             )
-            self.RECEIVE_DURATION = event_hub_config['RECEIVE_DURATION']
+            self.RECEIVE_DURATION = config['RECEIVE_DURATION']
         elif op == "send":
             # Create a producer client to send events to the event hub.
             self.producer = EventHubProducerClient.from_connection_string(
-                conn_str=event_hub_config['EVENT_HUB_CONN_STR'], eventhub_name=event_hub_config['EVENT_HUB_NAME']
+                conn_str=config['EVENT_HUB_CONN_STR'], eventhub_name=config['EVENT_HUB_NAME']
             )
-            self.SEND_COUNT = event_hub_config['SEND_COUNT']
+            self.SEND_COUNT = config['SEND_COUNT']
 
     def perform(self, op: str):
         if op == "receive":
