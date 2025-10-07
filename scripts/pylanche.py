5fefb2a85feed1839101a0fc8204a1cd307843b8
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-10-07 07:32:36 +0000

    Request to receive

--- scripts/pylanche.py
+++ scripts/pylanche.py
@@ -7,3 +7,7 @@ with open("./pylanche/config.json", "r") as config_file:
 
 app_name = config['APP_NAME']
 function_name = config['FUNCTION_NAME']
+
+url = "https://{}.azurewebsites.net/api/{}?operation=receive".format(app_name, function_name)
+
+resp = requests.post(url=url)
