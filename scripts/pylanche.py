e2d63e8b8635cb0fe9f66bec9f942bd709437ebe
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-10-07 11:05:32 +0000

    Request in while

--- scripts/pylanche.py
+++ scripts/pylanche.py
@@ -5,13 +5,15 @@ import requests
 with open("./pylanche/config.json", "r") as config_file:
     config = json.load(config_file)
 
+# Prepare the request URL to perform the receive operation.
 app_name = config['APP_NAME']
 function_name = config['FUNCTION_NAME']
 url = "https://{}.azurewebsites.net/api/{}?operation=receive".format(app_name, function_name)
 
-# Request to perform receive operation.
-try:
-    resp = requests.post(url=url)
-    print(resp.status_code)
-except Exception as error:
-    print(error)
+while True:
+    # Request to perform receive operation.
+    try:
+        resp = requests.post(url=url)
+        print(resp.status_code)
+    except Exception as error:
+        print(error)
