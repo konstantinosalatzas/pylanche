583f3e8c767bf801fd61d6bd170e0820d83ce815
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-10-10 09:36:27 +0000

    Get config values in with

--- pylanche/Client.py
+++ pylanche/Client.py
@@ -1,3 +1,4 @@
+import os
 import json
 
 from azure.eventhub.extensions.checkpointstoreblobaio import BlobCheckpointStore
@@ -12,26 +13,32 @@ class Client:
         # Read the configuration file.
         with open("./pylanche/config.json", "r") as config_file:
             config = json.load(config_file)
+            BLOB_STORAGE_CONNECTION_STRING = config['BLOB_STORAGE_CONNECTION_STRING']
+            BLOB_CONTAINER_NAME = config['BLOB_CONTAINER_NAME']
+            EVENT_HUB_CONNECTION_STRING = config['EVENT_HUB_CONNECTION_STRING']
+            EVENT_HUB_NAME = config['EVENT_HUB_NAME']
+            RECEIVE_DURATION = config['RECEIVE_DURATION']
+            SEND_COUNT = config['SEND_COUNT']
 
         if op == "receive":
             # Create an Azure blob checkpoint store to store the checkpoints.
             checkpoint_store = BlobCheckpointStore.from_connection_string(
-                config['BLOB_STORAGE_CONNECTION_STRING'], config['BLOB_CONTAINER_NAME']
+                BLOB_STORAGE_CONNECTION_STRING, BLOB_CONTAINER_NAME
             )
             # Create a consumer client to receive events from the event hub.
             self.consumer = EventHubConsumerClient.from_connection_string(
-                config['EVENT_HUB_CONNECTION_STRING'],
+                EVENT_HUB_CONNECTION_STRING,
                 consumer_group="$Default",
-                eventhub_name=config['EVENT_HUB_NAME'],
+                eventhub_name=EVENT_HUB_NAME,
                 checkpoint_store=checkpoint_store
             )
-            self.RECEIVE_DURATION = config['RECEIVE_DURATION']
+            self.RECEIVE_DURATION = RECEIVE_DURATION
         elif op == "send":
             # Create a producer client to send events to the event hub.
             self.producer = EventHubProducerClient.from_connection_string(
-                conn_str=config['EVENT_HUB_CONNECTION_STRING'], eventhub_name=config['EVENT_HUB_NAME']
+                conn_str=EVENT_HUB_CONNECTION_STRING, eventhub_name=EVENT_HUB_NAME
             )
-            self.SEND_COUNT = config['SEND_COUNT']
+            self.SEND_COUNT = SEND_COUNT
 
     def perform(self, op: str):
         if op == "receive":
