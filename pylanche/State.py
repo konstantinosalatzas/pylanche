e1697164cacfe3d7e72a080158c1f8c833e48ffc
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-04 09:03:48 +0000

    Update State

--- pylanche/State.py
+++ pylanche/State.py
@@ -1,4 +1,10 @@
 class State:
     def __init__(self, id):
         self.id = id
-        self.set = set()
+        self.events = {}
+
+    def update(self, event: dict):
+        state_id = self.id
+        state_events = self.events
+        if state_id in event:
+            state_events[event[state_id]] = event # upsert state
