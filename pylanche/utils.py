ee5fcf4d5d0a296f02e42a0a4ea84a947104c9e3
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-07 17:07:52 +0000

    Create get_config() in utils

--- pylanche/utils.py
+++ pylanche/utils.py
@@ -1,1 +1,14 @@
-# TODO: Refactor get_config() from Client and State
+import logging
+
+def get_config(config: dict[str, str]) -> tuple[str, ...]  | None:
+    try:
+        BLOB_STORAGE_CONNECTION_STRING = config['BLOB_STORAGE_CONNECTION_STRING']
+        BLOB_CONTAINER_NAME = config['BLOB_CONTAINER_NAME']
+        EVENT_HUB_CONNECTION_STRING = config['EVENT_HUB_CONNECTION_STRING']
+        EVENT_HUB_NAME = config['EVENT_HUB_NAME']
+        RECEIVE_DURATION = config['RECEIVE_DURATION']
+        SEND_COUNT = config['SEND_COUNT']
+    except Exception as error:
+        logging.info(str(error))
+        return None
+    return (BLOB_STORAGE_CONNECTION_STRING, BLOB_CONTAINER_NAME, EVENT_HUB_CONNECTION_STRING, EVENT_HUB_NAME, RECEIVE_DURATION, SEND_COUNT)
