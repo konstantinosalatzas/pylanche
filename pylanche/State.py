8256cbbde8802a0cd6e1f0b07de23a518e81ce44
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-07 17:44:32 +0000

    Refactor State config

--- pylanche/State.py
+++ pylanche/State.py
@@ -3,21 +3,12 @@ import json
 import logging
 import os
 
-from pylanche.utils import get_config
+from pylanche.utils import get_config_from_environ_or_file
 
 # Holds the event processing state of the execution.
 class State:
     def __init__(self):
-        # Get the environment variables.
-        config = get_config(os.environ)
-        if config == None:
-            logging.info("Failed to get the configuration values from the environment variables.")
-            # Read the configuration file.
-            with open("./pylanche/config.json", "r") as config_file:
-                config = get_config(json.load(config_file))
-                if config == None:
-                    logging.info("Failed to get the configuration values from the configuration file.")
-
+        config = get_config_from_environ_or_file()
         (BLOB_STORAGE_CONNECTION_STRING, BLOB_CONTAINER_NAME, EVENT_HUB_CONNECTION_STRING, EVENT_HUB_NAME, RECEIVE_DURATION, SEND_COUNT, STATE_ID) = config
         logging.info("Got the configuration values.")
 
