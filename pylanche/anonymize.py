0cbe597a3b20c2fc02bd92525348518a5398bc66
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-22 08:14:03 +0000

    Create recognize_names()

--- pylanche/anonymize.py
+++ pylanche/anonymize.py
@@ -1,5 +1,9 @@
 from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 
-def anonymize():
-    pass
+def recognize_names(client: TextAnalyticsClient, documents: list[str]):
+    print(documents)
+
+def anonymize(client: TextAnalyticsClient):
+    documents = ["I trained planche hold to press with Kostas."]
+    recognize_names(client, documents)
