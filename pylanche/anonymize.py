6116c13661616a73d45dcb3560e2ea408b9fef79
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-22 13:52:38 +0000

    Format prints

--- pylanche/anonymize.py
+++ pylanche/anonymize.py
@@ -7,22 +7,22 @@ def anonymize_text(text: str) -> str:
     return anonymized_text
 
 def recognize_names(client: TextAnalyticsClient, documents: list[str]):
-    print(documents)
     try:
         result = client.recognize_entities(documents=documents)[0]
         for entity in result['entities']:
-            print("Text: ", entity.text,
-                  "Category: ", entity.category,
-                  "SubCategory: ", entity.subcategory,
-                  "Confidence Score: ", round(entity.confidence_score, 2),
-                  "Length: ", entity.length,
-                  "Offset: ", entity.offset)
+            print("Text:", entity.text,
+                  "Category:", entity.category,
+                  "SubCategory:", entity.subcategory,
+                  "Confidence Score:", round(entity.confidence_score, 2),
+                  "Length:", entity.length,
+                  "Offset:", entity.offset)
             if entity.category == "Person":
                 anonymized_text = anonymize_text(entity.text)
-                print(entity.text, anonymized_text)
+                print(entity.text, "->", anonymized_text)
     except Exception as error:
         logging.info(str(error))
 
 def anonymize(client: TextAnalyticsClient):
     documents = ["I trained planche hold to press with Konstantinos."]
+    print(documents)
     recognize_names(client, documents)
