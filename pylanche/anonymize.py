1fe0374c316bde419933050f4a3712989e818d36
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-22 08:19:18 +0000

    Recognize entities in text

--- pylanche/anonymize.py
+++ pylanche/anonymize.py
@@ -1,8 +1,18 @@
 from azure.ai.textanalytics import TextAnalyticsClient
-from azure.core.credentials import AzureKeyCredential
 
 def recognize_names(client: TextAnalyticsClient, documents: list[str]):
     print(documents)
+    try:
+        result = client.recognize_entities(documents=documents)[0]
+        for entity in result['entities']:
+            print("Text: ", entity.text,
+                  "Category: ", entity.category,
+                  "SubCategory: ", entity.subcategory,
+                  "Confidence Score: ", round(entity.confidence_score, 2),
+                  "Length: ", entity.length,
+                  "Offset: ", entity.offset)
+    except Exception as error:
+        print(error)
 
 def anonymize(client: TextAnalyticsClient):
     documents = ["I trained planche hold to press with Kostas."]
