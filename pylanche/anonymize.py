a1ab3b5f49ecf0b0beec162d6e14bcf45424f322
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-23 11:38:46 +0000

    Create names dict

--- pylanche/anonymize.py
+++ pylanche/anonymize.py
@@ -6,10 +6,9 @@ def anonymize_text(text: str) -> str:
     anonymized_text = "X"*len(text) # Replace all letters with "X".
     return anonymized_text
 
-def recognize_names(client: TextAnalyticsClient, text: str) -> dict[str, str]:
+def recognize_names(client: TextAnalyticsClient, text: str) -> dict[str, str] | None:
     try:
-        documents = [text]
-        result = client.recognize_entities(documents=documents)[0]
+        result = client.recognize_entities(documents=[text])[0]
         anonymization = {} # Map names to their anonymized forms.
         for entity in result['entities']:
             print("Text:", entity.text,
@@ -25,8 +24,9 @@ def recognize_names(client: TextAnalyticsClient, text: str) -> dict[str, str]:
         return anonymization
     except Exception as error:
         logging.info(str(error))
-        return {}
+        return None
 
 def anonymize(client: TextAnalyticsClient):
     text = "I trained planche hold to press with Konstantinos."
-    recognize_names(client, text)
+    names = recognize_names(client, text)
+    print(names)
