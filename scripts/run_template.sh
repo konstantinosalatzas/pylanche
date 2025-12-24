90d3396d2d3bd0b2cf81ec56b056a8be452bf031
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-24 12:56:24 +0000

    Anonymize with shell script

--- scripts/run_template.sh
+++ scripts/run_template.sh
@@ -3,3 +3,6 @@ curl -X POST https://<APP_NAME>.azurewebsites.net/api/<FUNCTION_NAME>?operation=
 
 # send
 curl -X POST https://<APP_NAME>.azurewebsites.net/api/<FUNCTION_NAME>?operation=send
+
+# anonymize
+curl -X POST https://<APP_NAME>.azurewebsites.net/api/<FUNCTION_NAME>?operation=anonymize&text=<TEXT>
