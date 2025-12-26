47805817d7824bfbca46dd7b834fc36ca3e0aa7d
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-26 15:38:30 +0000

    Refactor input parameter and returned value

--- function_app.py
+++ function_app.py
@@ -27,32 +27,26 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
                                  status_code=400)
 
     if op == "receive":
-        duration = get_parameter(req, 'duration')
+        param = get_parameter(req, 'duration')
     if op == "send":
-        count = get_parameter(req, 'count')
+        param = get_parameter(req, 'count')
     if op == "anonymize":
-        text = get_parameter(req, 'text')
+        param = get_parameter(req, 'text')
 
     try:
         # Create a client and perform the operation.
         client = pylanche.Client(op)
-        
-        if op == "receive":
-            client.perform(op, duration)
-        if op == "send":
-            client.perform(op, count)
-        if op == "anonymize":
-            anonymized_text = client.perform(op, text)
+        ret = client.perform(op, param)
         
         if op == "receive":
             return func.HttpResponse("The function received events from the event hub.")
         if op == "send":
             return func.HttpResponse("The function sent events to the event hub.")
         if op == "anonymize":
-            if anonymized_text == None:
+            if ret == None:
                 return func.HttpResponse("The function failed to perform the operation, please check the logs.",
                                          status_code=500)
-            return func.HttpResponse(anonymized_text)
+            return func.HttpResponse(ret)
     except Exception as error:
         logging.error(str(error))
         return func.HttpResponse("The function failed to perform the operation, please check the logs.",
