f60d71c05746def745796760496e01e408b18064
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-26 15:18:07 +0000

    Update curl

--- scripts/run_template.sh
+++ scripts/run_template.sh
@@ -1,5 +1,5 @@
 # receive
-curl -X POST https://<APP_NAME>.azurewebsites.net/api/<FUNCTION_NAME>?operation=receive
+curl -X POST https://<APP_NAME>.azurewebsites.net/api/<FUNCTION_NAME>?operation=receive&duration=3
 
 # send
 curl -X POST https://<APP_NAME>.azurewebsites.net/api/<FUNCTION_NAME>?operation=send&count=3
