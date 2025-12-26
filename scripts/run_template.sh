26409efc3b7df8f972a7c6a16ded10afbbb62f5b
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-12-26 13:45:12 +0000

    Update curl

--- scripts/run_template.sh
+++ scripts/run_template.sh
@@ -2,7 +2,7 @@
 curl -X POST https://<APP_NAME>.azurewebsites.net/api/<FUNCTION_NAME>?operation=receive
 
 # send
-curl -X POST https://<APP_NAME>.azurewebsites.net/api/<FUNCTION_NAME>?operation=send
+curl -X POST https://<APP_NAME>.azurewebsites.net/api/<FUNCTION_NAME>?operation=send&count=3
 
 # anonymize
 curl -X POST https://<APP_NAME>.azurewebsites.net/api/<FUNCTION_NAME>?operation=anonymize&text=<TEXT>
